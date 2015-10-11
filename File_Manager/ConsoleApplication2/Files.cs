6e9675346bff96962d2ba017072f73c75adac1fc
Hakerok <Hakerok1995@gmail.com>
2015-10-11 13:30:43 +0700

    Working with Files

---
+++ File_Manager/ConsoleApplication2/Files.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+
+namespace ConsoleApplication2
+{
+    class Files: UltimateFilesHurricaneManagerClassLibrary.Node
+    {
+        public string Per;
+        public string NewPer;
+        public IEnumerable<int> _information()
+        {
+            //Проверка диска на доступность
+          var d = new DriveInfo(Per);
+            if (d.IsReady)
+            {
+                // Отображение всех файлов по пути
+                string[] path = Directory.GetFiles(Per);
+                for (var i = 0; i < 1; i++)
+                {
+                    foreach (var s in path)
+                    {
+                        var inform = new FileInfo(s);
+                        Name = inform.Name;
+                        Path = inform.DirectoryName;
+                        Size = inform.Length;
+                        DateOfCreation = File.GetCreationTime(s);
+                        DateOfChange = File.GetLastAccessTimeUtc(s);
+                        DateOfLastAccess = File.GetLastAccessTime(s);
+                        yield return i;
+                    }
+                }
+            }
+
+        }
+        public override void CopyFile()
+        {
+            FileInfo fileInf = new FileInfo(Per);
+            if (fileInf.Exists)
+            {
+                // чтение из файла
+                using (FileStream fstream = File.OpenRead(Per))
+                {
+                    // преобразуем строку в байты
+                    byte[] array = new byte[fstream.Length];
+                    // считываем данные
+                    fstream.Read(array, 0, array.Length);
+                    fileInf.CopyTo(NewPer, true);
+                }
+            }
+        }
+        public override void WriteBytes()
+        {
+            using (FileStream fstream = new FileStream(Per, FileMode.OpenOrCreate))
+            {
+                // преобразуем строку в байты
+                byte[] array = System.Text.Encoding.Default.GetBytes(Per);
+                // запись массива байтов в файл
+                fstream.Write(array, 0, array.Length);
+
+            }
+        }
+        public override void ReplaceFile()
+        {
+            FileInfo fileInf = new FileInfo(Per);
+            if (fileInf.Exists)
+            { // чтение из файла
+                using (FileStream fstream = File.OpenRead(Per))
+                {
+                    // преобразуем строку в байты
+                    var array = new byte[fstream.Length];
+                    // считываем данные
+                    fstream.Read(array, 0, array.Length);
+                    fileInf.MoveTo(NewPer);
+                }
+          }
+        }
+        public override void RemoveFile()
+        {
+            //Проверка на существование файла
+            if (!File.Exists(Per)) return;
+            //Обрабатываем если удаление невозможно
+            try
+            {
+                File.Delete(Per);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Защищено от записи", "Невозможно удалить файл", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        public override void OpenFile()
+        {
+            //Проверка на существование файла
+            if (!File.Exists(Per)) return;
+            //Открываем файл внешней программой
+           var p1 = new Process { StartInfo = { FileName = Per } };
+                p1.Start(); 
+            
+        }
+
+      
+    }
+}
---
+++ File_Manager/ConsoleApplication2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            int i;
+            do
+            {
+                Console.Write(
+                    "Меню:\n1) Просмотр файлов  \n2) Копировать \n3) заменить \n4) удалить \n5) Открыть \n6) Выйти из программы\n\nВаше решение: ");
+                // ReSharper disable once AssignNullToNotNullAttribute
+                i = int.Parse(Console.ReadLine());
+                Files files ;
+                switch (i)
+                {
+                    case 1:
+                        Console.Clear();
+                        files = new Files();
+                        Console.Write("Введите путь");
+                        files.Per = Console.ReadLine();                
+                        // ReSharper disable once UnusedVariable
+                        foreach (var i1 in files._information())
+                        {
+                            Console.WriteLine(files.Name);
+                            Console.WriteLine(files.Path);
+                            Console.WriteLine(files.Size);
+                            Console.WriteLine(files.DateOfChange);
+                            Console.WriteLine(files.DateOfCreation);
+                            Console.WriteLine(files.DateOfLastAccess);
+                            Console.WriteLine("___________________________________________");
+                            
+                        }
+                        break;
+                    case 2:
+                        files = new Files();
+                        Console.WriteLine("Файл который необходимо скопировать");
+                        files.Per = Console.ReadLine();
+                        files.WriteBytes();
+                        Console.WriteLine("Куда скопировать файл");
+                        files.NewPer = Console.ReadLine();
+                        files.CopyFile();
+                        break;
+                    case 3:
+                        files = new Files();
+                        Console.WriteLine("Файл который необходимо переместить");
+                        files.Per = Console.ReadLine();
+                        files.WriteBytes();
+                        Console.WriteLine("Куда переместить файл файл");
+                        files.NewPer = Console.ReadLine();
+                        files.ReplaceFile();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("Путь к файлу");
+                        files = new Files{Per = Console.ReadLine()};
+                        files.RemoveFile();
+                        Console.WriteLine("Удалено");
+                        break;
+                    case 5:
+                        Console.Clear();
+                        Console.WriteLine("Путь к файлу");
+                        files = new Files {Per = Console.ReadLine()};
+                        files.OpenFile();
+                        break;
+                    case 6:
+                        
+                        break;
+                    default:
+                        Console.WriteLine("Вы что-то другое нажали...");
+                        break;
+                }
+                Console.Write("\n\n\t\t\tНажмите любую клавишу...");
+                Console.ReadLine();
+                Console.Clear();
+            } while (i != 6);
+        }
+    }
+}
